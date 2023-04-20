94e1fb8346fa693fabbe648e0e6edd20e48829a8
Aadya21370 <aadya21370@iiitd.ac.in>
2023-04-20 16:37:18 +0530

    up

--- static/js/card.js
+++ static/js/card.js
@@ -66,24 +66,24 @@ dropdownItems.forEach(item => {
   item.addEventListener('click', () => {
     const selectedValue = item.getAttribute('data-value');
     if (selectedValue === 'Monthly') {
-      expiresOnField.innerHTML = 'Expires on: <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">' + new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toLocaleDateString() + '</span>';
-      priceField.innerHTML = 'Price: <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">Rs.100 per month</span>';
+      expiresOnField.innerHTML = 'Expires on <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">' + new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toLocaleDateString() + '</span>';
+      priceField.innerHTML = 'Price <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">Rs.100 per month</span>';
     } else if (selectedValue === 'Quaterly') {
-      expiresOnField.innerHTML = 'Expires on: <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">' + new Date(Date.now() + 3 * 30 * 24 * 60 * 60 * 1000).toLocaleDateString() + '</span>';
-      priceField.innerHTML = 'Price: <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">Rs.270 per quarter</span>';
+      expiresOnField.innerHTML = 'Expires on <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">' + new Date(Date.now() + 3 * 30 * 24 * 60 * 60 * 1000).toLocaleDateString() + '</span>';
+      priceField.innerHTML = 'Price <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">Rs.270 per quarter</span>';
     } else if (selectedValue === 'Yearly') {
-      expiresOnField.innerHTML = 'Expires on: <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">' + new Date(Date.now() + 365 * 24 * 60 * 60 * 1000).toLocaleDateString() + '</span>';
-      priceField.innerHTML = 'Price: <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">Rs.1000 per year</span>';
+      expiresOnField.innerHTML = 'Expires on <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">' + new Date(Date.now() + 365 * 24 * 60 * 60 * 1000).toLocaleDateString() + '</span>';
+      priceField.innerHTML = 'Price <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">Rs.1000 per year</span>';
     }
   });
 });
 
-expiresOnField.textContent = 'Expires on:';
-priceField.textContent = 'Price:';
+expiresOnField.textContent = 'Expires on';
+priceField.textContent = 'Price';
 
 // Membership Card
 function toggleCard3() {
-  var overlay = document.getElementById("overla32");
+  var overlay = document.getElementById("overlay3");
   var card = document.getElementById("profile-card3");
   overlay.style.display = "block";
   card.style.display = "block";
@@ -97,3 +97,52 @@ noButton3.addEventListener("click", function() {
   overlay3.style.display = "none";
   card3.style.display = "none";
 });
+
+const dropdown2 = document.querySelector('.dropdown');
+const dropdownToggle2 = dropdown2.querySelector('.dropdown-toggle');
+const dropdownMenu2 = dropdown2.querySelector('.dropdown-menu');
+
+dropdownToggle2.addEventListener('click', function() {
+  dropdownMenu2.classList.toggle('show');
+});
+
+dropdownMenu2.addEventListener('click', function(event) {
+  event.preventDefault();
+  const target = event.target;
+  if (target.matches('.dropdown-item')) {
+    dropdownToggle2.textContent = target.textContent;
+    dropdownToggle2.dataset.value = target.dataset.value;
+  }
+});
+
+document.addEventListener('click', function(event) {
+  const isClickInsideDropdown = dropdown2.contains(event.target);
+  if (!isClickInsideDropdown) {
+    dropdownMenu2.classList.remove('show');
+  }
+});
+
+
+const dropdownButton2 = document.getElementById('dropdownMenuButton');
+const expiresOnField2 = document.querySelector('.features');
+const priceField2 = document.querySelector('.p_price');
+
+const dropdownItems2 = document.querySelectorAll('.dropdown-item');
+dropdownItems2.forEach(item => {
+  item.addEventListener('click', () => {
+    const selectedValue = item.getAttribute('data-value');
+    if (selectedValue === 'Basic') {
+      expiresOnField2.innerHTML = 'Features <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">5% Discounts</span>';
+      priceField2.innerHTML = 'Price <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">Rs.300 per year</span>';
+    } else if (selectedValue === 'Standard') {
+      expiresOnField2.innerHTML = 'Features <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">5% Discounts + VIP Coupons</span>';
+      priceField2.innerHTML = 'Price <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">Rs.600 per year</span>';
+    } else if (selectedValue === 'Premium') {
+      expiresOnField2.innerHTML = 'Features <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">5% Discounts + VIP Coupons + Free Delivery</span>';
+      priceField2.innerHTML = 'Price <br><span style="color: #6c757d; font-weight: bold; font-size:13px;">Rs.900 per year</span>';
+    }
+  });
+});
+
+expiresOnField2.textContent = 'Features';
+priceField2.textContent = 'Price';
