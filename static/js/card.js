38c250a46e5494b81fd0e1cf392ba0beb96a7432
Aadya21370 <aadya21370@iiitd.ac.in>
2023-04-20 09:33:21 +0530

    updates

---
+++ static/js/card.js
@@ -0,0 +1,20 @@
+function toggleCard() {
+    var overlay = document.getElementById("overlay");
+    var card = document.getElementById("profile-card");
+    if (card.style.display === "none" || overlay.style.display === "none") {
+      overlay.style.display = "block";
+      card.style.display = "block";
+    } else {
+      overlay.style.display = "none";
+      card.style.display = "none";
+    }
+  }
+
+const noButton = document.getElementById("no");
+const overlay = document.getElementById("overlay");
+const card = document.getElementById("profile-card");
+
+noButton.addEventListener("click", function() {
+    overlay.style.display = "none";
+    card.style.display = "none";
+});
